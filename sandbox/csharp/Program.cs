9558d14fdf2c314bada341544c5a019e6d6cd010
Ben Smyth <smythjben@gmail.com>
2024-05-02 01:54:50 -0400

    Play with async

--- sandbox/csharp/Program.cs
+++ sandbox/csharp/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello ");
+await Task.Delay(1000);
+Console.WriteLine("World!");
