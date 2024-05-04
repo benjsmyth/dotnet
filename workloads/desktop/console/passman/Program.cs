80cbea3e91f8d48ffd58c1781baa1949f6ed427a
Ben Smyth <smythjben@gmail.com>
2024-05-04 01:57:38 -0400

    Add some classes

--- workloads/desktop/console/passman/Program.cs
+++ workloads/desktop/console/passman/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("PassMan | Password manager");
+﻿Console.WriteLine("Passman | Password manager");
+
+sealed class Database
+{
+    public interface IDatabase
+    {
+    
+    }
+
+    public static Password? Find(string password)
+    {
+        return null;
+    }
+}
+
+abstract class Generator : Database.IDatabase
+{
+    private readonly Password? password;
+    public static Password Generate()
+    {
+        string password = "generatedPassword";
+        return new Password(password);
+    }
+}
+
+sealed class Password(string password)
+{
+    private readonly string? value = password;
+}
