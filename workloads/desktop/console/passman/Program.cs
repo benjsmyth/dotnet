31ffc380a5d522d01031530e4582ef36d79048ba
Ben Smyth <smythjben@gmail.com>
2024-05-08 23:40:53 -0400

    Implement program loop

--- workloads/desktop/console/passman/Program.cs
+++ workloads/desktop/console/passman/Program.cs
@@ -1,29 +1,87 @@
-﻿Console.WriteLine("Passman | Password manager");
+﻿Console.WriteLine("PassMan | Password manager");
+Manager.Run();
 
-sealed class Database
+abstract class Manager
 {
-    public interface IDatabase
+    public static void Run()
     {
-    
+        uint action;
+        while ( (action = Prompt() ) > 0)
+        {
+            Execute(action);
+        }
     }
-
-    public static Password? Find(string password)
+    public static uint Prompt()
     {
-        return null;
+        Console.WriteLine("\t1: Search");
+        Console.WriteLine("\t2: Edit");
+        Console.WriteLine("\t3: Load");
+        Console.WriteLine("\t4: Save");
+        Console.WriteLine("\t5: Reset");
+        Console.WriteLine("\tQ: Quit");
+        return Select();
+    }
+    private static uint Select()
+    {
+        Console.Write("Select: ");
+        try
+        {
+            string? value = Console.ReadLine();
+            return Convert.ToUInt16(value);
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("IOException. Quitting");
+            return 0;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Quitting");
+            return 0;
+        }
+    }
+    private static void Execute(uint action)
+    {
+        Console.WriteLine($"Executing {action}");
     }
 }
 
-abstract class Generator : Database.IDatabase
-{
-    private readonly Password? password;
-    public static Password Generate()
-    {
-        string password = "generatedPassword";
-        return new Password(password);
-    }
-}
+// sealed class Database : ISortable
+// {
+//     private readonly string _Path;
+//     private readonly Stream _Stream;
+//     private readonly BinaryWriter _Writer;
 
-sealed class Password(string password)
-{
-    private readonly string? value = password;
-}
+//     public Database(string path)
+//     {
+//         _Path = @path;
+//         // Check for nullability!
+        
+//         _Stream = File.Open(_Path, FileMode.OpenOrCreate);
+//         // Remember to close!
+
+//         _Writer = new BinaryWriter(_Stream, Encoding.UTF8);
+//         // Remember to close!
+//     }
+//     public static void Sort()
+//     {
+//         return;
+//     }
+// }
+// abstract class Generator : ISortable
+// {
+//     public static readonly Password[] passwords = [];
+//     public static Password Generate()
+//     {
+//         return new("", []);
+//     }
+//     public static void Sort()
+//     {
+//         return;
+//     }
+// }
+// interface ISortable
+// {
+//     abstract static void Sort();
+// }
+// record Password(string Value, string[] Keys);
--- workloads/desktop/console/passman/obj/Debug/net8.0/passman.AssemblyInfo.cs
+++ workloads/desktop/console/passman/obj/Debug/net8.0/passman.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("passman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7934edb98b1b7e09eecb2804a1d22c5349991a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80cbea3e91f8d48ffd58c1781baa1949f6ed427a")]
 [assembly: System.Reflection.AssemblyProductAttribute("passman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("passman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
