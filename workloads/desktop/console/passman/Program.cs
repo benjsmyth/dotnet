c03198ee9e1110c9dd6bf191207db20139729863
Ben Smyth <smythjben@gmail.com>
2024-05-09 04:09:36 -0400

    Simplify menu

--- workloads/desktop/console/passman/Program.cs
+++ workloads/desktop/console/passman/Program.cs
@@ -1,73 +1,80 @@
-﻿Console.WriteLine("PassMan | Password manager");
-Manager.Work();
+﻿Manager.Work();
 
-abstract class Manager
+static class Manager
 {
-    private static readonly Database? _Database;
+    private static Database? _database;
 
     public static void Work()
     {
         uint action;
-        while ( (action = Prompt() ) > 0)
+        while ( (action = Select() ) > 0)
         {
             Execute(action);
         }
         Console.WriteLine("Quitting");
     }
-    private static uint Prompt()
+
+    private static uint Select()
     {
-        Console.WriteLine("""
-            1: Search
-            2: Edit
-            3: Load
-            4: Save
-            5: Reset
-            Q: Quit
-        """);
-        Console.Write("Select: ");
+        Prompt();
         try
         {
             string? value = Console.ReadLine();
             return Convert.ToUInt16(value);
         }
-        catch (IOException)
-        {
-            Console.Write("IOException. ");
-            return 0;
-        }
         catch (FormatException)
         {
+            // Quit
             return 0;
         }
     }
+
+    private static void Prompt()
+    {
+        Console.Write(string.Join(" | ",
+            null, "Search", "Edit", "Load", "Save", "Create", "Destroy", "Quit", null)
+        );
+    }
+
     private static void Execute(uint action)
     {
-        Console.WriteLine($"Executing {action}");
+        switch (action)
+        {
+            case 5:
+                Console.Write("Pathname: ");
+                string? path = Console.ReadLine();
+                _database = new(path);
+                break;
+
+            default:
+                break;
+        }
     }
 }
 
 sealed class Database : ISortable
 {
     private readonly string _Path;
-    private readonly Stream _Stream;
-    private readonly BinaryWriter _Writer;
+    // private readonly Stream _Stream;
+    // private readonly BinaryWriter _Writer;
 
-    public Database(string path)
+    public Database(string? path)
     {
-        _Path = @path;
-        // Check for nullability!
-        
-        _Stream = File.Open(_Path, FileMode.OpenOrCreate);
+        _Path = path!;
+        Console.WriteLine($"Created database at {_Path}");
+        // _Stream = File.Open(_Path, FileMode.OpenOrCreate);
         // Remember to close!
 
-        _Writer = new BinaryWriter(_Stream, System.Text.Encoding.UTF8);
+        // _Writer = new BinaryWriter(_Stream, System.Text.Encoding.UTF8);
         // Remember to close!
     }
+
     public static void Sort()
     {
         return;
     }
 }
+
 // abstract class Generator : ISortable
 // {
 //     public static readonly Password[] passwords = [];
@@ -80,8 +87,10 @@ sealed class Database : ISortable
 //         return;
 //     }
 // }
+
 interface ISortable
 {
     abstract static void Sort();
 }
+
 // record Password(string Value, string[] Keys);
